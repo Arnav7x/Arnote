b7bf0f9bd9e7af616c760917a6ef3dce18ed324d
Arnav <arnav777x@gmail.com>
2025-10-22 23:00:16 +0530

    first

---
+++ backend/src/controllers/notesController.js
@@ -0,0 +1,35 @@
+import Note from '../models/Note.js';
+
+export async function getAllNotes(req, res) {
+    try {
+        const notes = await Note.find();
+
+        res.status(200).json(notes);
+    } catch (error) {
+        console.error("Error in getAllNotes controller", error);
+
+        res.status(500).json({message: "Internal Server Error."});
+    }
+};
+
+export async function createNote(req, res) {
+    try {
+        const {title, content} = req.body;
+        const newNote = new Note({title, content});
+
+        await newNote.save();
+
+        res.status(201).json(newNote);
+    } catch (error) {
+        console.error("Error in createNote controller:", error);
+
+        res.status(500).json({message: "Internal Server Error."});
+    }
+};
+
+export function updateNote(req, res) {
+};
+
+export function deleteNote(req, res) {
+};
+
